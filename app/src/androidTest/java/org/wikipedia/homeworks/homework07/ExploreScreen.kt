936c8cfe7c51106e4da5e0e123cc77c1167dd6f4
st412m <staroverm@gmail.com>
2025-02-18 21:08:27 +0300

    classwork Recycler

---
+++ app/src/androidTest/java/org/wikipedia/homeworks/homework07/ExploreScreen.kt
@@ -0,0 +1,26 @@
+package org.wikipedia.homeworks.homework07
+
+import com.kaspersky.kaspresso.screens.KScreen
+import io.github.kakaocup.kakao.image.KImageView
+import io.github.kakaocup.kakao.recycler.KRecyclerView
+import io.github.kakaocup.kakao.text.KTextView
+import org.wikipedia.R
+import org.wikipedia.feed.view.FeedView
+
+object ExploreScreen : KScreen<ExploreScreen>(){
+    override val layoutId = R.layout.fragment_feed
+    override val viewClass = FeedView ::class.java
+
+    val toolbarTitle = KImageView{
+        withId(R.id.main_toolbar_wordmark)
+    }
+
+    val items = KRecyclerView(
+        builder = {
+            withId(R.id.feed_view)
+        },
+        itemTypeBuilder = {
+            itemType(::SearchCardViewItem)
+        }
+    )
+}
