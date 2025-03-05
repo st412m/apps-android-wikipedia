5c55f5f4ca420c553d98c941b1e5afd75e188ee9
st412m <staroverm@gmail.com>
2025-03-05 15:59:46 +0300

    homework10

--- app/src/androidTest/java/org/wikipedia/homeworks/homework10/OnboardingUiAutomatorTest.kt
+++ app/src/androidTest/java/org/wikipedia/homeworks/homework10/OnboardingUiAutomatorTest.kt
@@ -4,19 +4,59 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import org.junit.Rule
 import org.junit.Test
+import org.wikipedia.homeworks.homework03.pageIndicator
 import org.wikipedia.main.MainActivity
 
-class OnboardingUiAutomatorTest : TestCase(){
+class OnboardingUiAutomatorTest : TestCase() {
 
     @get:Rule
 
     val activityScenarioRule: ActivityScenarioRule<MainActivity> =
         ActivityScenarioRule(MainActivity::class.java)
 
+    private val numSwipes = 3
+
     @Test
-    fun checkImage(){
+    fun checkMainBlock() {
+        repeat(numSwipes) {
+            run {
+                step("Проверяем наличие изображения") {
+                    OnboardingUIScreen.image.isDisplayed()
+                }
+                step("Проверяем наличие заголовка") {
+                    OnboardingUIScreen.primaryTitle.isDisplayed()
+                }
+                step("Проверям наличие описания") {
+                    OnboardingUIScreen.secondaryTitle.isDisplayed()
+                }
+                step("Свайпаем на следующую страницу") {
+                    OnboardingUIScreen.viewPager.swipeLeft()
+                }
+            }
+        }
+    }
+
+    @Test
+    fun checkLanguageBlock() {
         run {
-            OnboardingUIScreen.image.isDisplayed()
+            step("Проверяем отображение на первом экране блока языков") {
+                OnboardingUIScreen.languageBlock.isEnabled()
+            }
+        }
+    }
+    @Test
+    fun checkNavigationButtons(){
+        repeat(numSwipes){index ->
+            run {
+                step("Проверяем наличие кнопки 'Skip'") {
+                    OnboardingUIScreen.skipButton.isDisplayed()
+                    OnboardingUIScreen.skipButton.isClickable()
+                }
+                step("Проверяем наличие блока отображения страницы"){
+                    OnboardingUIScreen.pageIndicator.isDisplayed()
+
+                }
+            }
         }
     }
 }
