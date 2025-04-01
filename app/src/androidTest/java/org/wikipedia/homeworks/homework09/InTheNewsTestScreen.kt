f630b4be8c4b1efb830a05d4ad567b062c99fa59
st412m <staroverm@gmail.com>
2025-04-01 21:11:18 +0300

    AGP 8.9.1

--- app/src/androidTest/java/org/wikipedia/homeworks/homework09/InTheNewsTestScreen.kt
+++ app/src/androidTest/java/org/wikipedia/homeworks/homework09/InTheNewsTestScreen.kt
@@ -1,7 +1,10 @@
 package org.wikipedia.homeworks.homework09
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.kaspersky.components.alluresupport.withForcedAllureSupport
+import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import org.junit.After
 import org.junit.Rule
 import org.junit.Test
 import org.wikipedia.homeworks.homework07.ExploreScreen
@@ -10,7 +13,7 @@ import org.wikipedia.homeworks.homework07.NewsCardItem
 import org.wikipedia.homeworks.homework08.OnboardingScreen
 import org.wikipedia.main.MainActivity
 
-class InTheNewsTestScreen : TestCase() {
+class InTheNewsTestScreen : TestCase(Kaspresso.Builder.withForcedAllureSupport()) {
 
     @get:Rule
     val activityScenarioRule: ActivityScenarioRule<MainActivity> =
