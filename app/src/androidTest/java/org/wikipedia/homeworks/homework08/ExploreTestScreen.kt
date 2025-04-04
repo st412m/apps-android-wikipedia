08fcee54506b0052501651d3aceb04ee6c235ecb
st412m <staroverm@gmail.com>
2025-04-04 09:35:06 +0300

    удалил лишние импорты

--- app/src/androidTest/java/org/wikipedia/homeworks/homework08/ExploreTestScreen.kt
+++ app/src/androidTest/java/org/wikipedia/homeworks/homework08/ExploreTestScreen.kt
@@ -1,14 +1,12 @@
 package org.wikipedia.homeworks.homework08
 
 import androidx.test.espresso.Espresso
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.kaspersky.components.alluresupport.withForcedAllureSupport
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import org.junit.Rule
 import org.junit.Test
-import org.wikipedia.homeworks.homework03.languagesList
 import org.wikipedia.main.MainActivity
 
 class ExploreTestScreen : TestCase(Kaspresso.Builder.withForcedAllureSupport()) {
@@ -25,7 +23,7 @@ class ExploreTestScreen : TestCase(Kaspresso.Builder.withForcedAllureSupport()) 
             var page = 0
             repeat(numOfPage) {
                 step("Проверяем отображение логотипа") {
-                    OnboardingScreen.slider.childAt<OnboardingPagerItem>(page) {
+                    OnboardingScreen.slider.childAt<OnboardingPagerItem>(1) {
                         wikiLogo.isDisplayed()
                     }
                 }
@@ -43,7 +41,7 @@ class ExploreTestScreen : TestCase(Kaspresso.Builder.withForcedAllureSupport()) 
             var page = 0
             repeat(numOfPage) {
                 step("Проверяем отображение заглавного текстового поля") {
-                    OnboardingScreen.slider.childAt<OnboardingPagerItem>(page) {
+                    OnboardingScreen.slider.childAt<OnboardingPagerItem>(1) {
                         textTitle.isDisplayed()
                     }
                 }
@@ -61,7 +59,7 @@ class ExploreTestScreen : TestCase(Kaspresso.Builder.withForcedAllureSupport()) 
             var page = 0
             repeat(numOfPage) {
                 step("Проверяем отображение второго тестового поля") {
-                    OnboardingScreen.slider.childAt<OnboardingPagerItem>(page) {
+                    OnboardingScreen.slider.childAt<OnboardingPagerItem>(1) {
                         secondTextTitle.isDisplayed()
                     }
                 }
@@ -89,113 +87,113 @@ class ExploreTestScreen : TestCase(Kaspresso.Builder.withForcedAllureSupport()) 
         }
     }
 
-//    @Test
-//    fun bottomButtonSkipTest() {
-//        run {
-//            step("Проверяем отображение кнопки 'Skip'") {
-//                OnboardingScreen.skipButton.isDisplayed()
-//            }
-//
-//            step("Проверяем кликабельность кнопки 'Skip'") {
-//                OnboardingScreen.skipButton.isClickable()
-//            }
-//        }
-//    }
-//
-//    @Test
-//    fun pageIndicatorTest() {
-//        run {
-//            step("Наличие индикатора страниц") {
-//                OnboardingScreen.pageIndicator.isDisplayed()
-//            }
-//        }
-//    }
-//
-//    @Test
-//    fun bottomButtonContinueTest() {
-//        run {
-//            step("Проверяем отображение кнопки 'Skip'") {
-//                OnboardingScreen.forwardButton.isDisplayed()
-//            }
-//
-//            step("Проверяем кликабельность кнопки 'Skip'") {
-//                OnboardingScreen.forwardButton.isClickable()
-//            }
-//        }
-//    }
-//
-//    @Test
-//    fun bottomButtonGetStartedTest() {
-//        run {
-//            step("Убеждаемся, что ее нет на начальном экране") {
-//                OnboardingScreen.doneButton.isGone()
-//            }
-//
-//            step("Делаем 3 клика до ее появления") {
-//                repeat(numOfPage) {
-//                    OnboardingScreen.forwardButton.click()
-//                }
-//            }
-//
-//            step("Проверяем отображение кнопки 'Get Started'") {
-//                OnboardingScreen.doneButton.isDisplayed()
-//            }
-//
-//            step("Проверяем кликабельность кнопки 'Get Started'") {
-//                OnboardingScreen.doneButton.isClickable()
-//            }
-//        }
-//    }
-//
-//    @Test
-//    fun addLanguageTest() {
-//        run {
-//            step("Нажимаем на кнопку 'Add or edit languge'") {
-//                OnboardingScreen.slider.childAt<OnboardingPagerItem>(0) {
-//                    addLanguageButton.click()
-//                }
-//            }
-//            step("Проверяем, что открылся экран выбора языков") {
-//                WikipediaLanguagesScreen.toolbar.isDisplayed()
-//            }
-//
-//            step("Проверяем наличие списка языков") {
-//                WikipediaLanguagesScreen.languageList.isDisplayed()
-//            }
-//
-//            step("Проверяем наличие кнопки 'Add language'") {
-//                WikipediaLanguagesScreen.addLanguageButton.isDisplayed()
-//            }
-//            step("Кликаем кнопку 'Add language'") {
-//                WikipediaLanguagesScreen.addLanguageButton.click()
-//            }
-//            step("Проверяем, что открылся список языков") {
-//                AddLanguageScreen {
-//                    languageList.childAt<ListLanguageItem>(3) {
-//                        localizedLanguageName.isDisplayed()
-//                        languageSubtitle.isDisplayed()
-//                    }
-//                }
-//            }
-//            step("Кликаем по языку") {
-//                AddLanguageScreen {
-//                    languageList.childAt<ListLanguageItem>(3) {
-//                        localizedLanguageName.click()
-//                    }
-//                }
-//            }
-//            step("Возвращаемся на главный экран") {
-//                Espresso.pressBack()
-//            }
-//            step("Проверяем, что язык отобразился на главном экране") {
-//                OnboardingScreen.slider.childAt<OnboardingPagerItem>(0) {
-//                    languages.childAt<OptionLanguageItem>(2) {
-//                        containsText("Deutsch")
-//                    }
-//                }
-//            }
-//        }
-//    }
+    @Test
+    fun bottomButtonSkipTest() {
+        run {
+            step("Проверяем отображение кнопки 'Skip'") {
+                OnboardingScreen.skipButton.isDisplayed()
+            }
+
+            step("Проверяем кликабельность кнопки 'Skip'") {
+                OnboardingScreen.skipButton.isClickable()
+            }
+        }
+    }
+
+    @Test
+    fun pageIndicatorTest() {
+        run {
+            step("Наличие индикатора страниц") {
+                OnboardingScreen.pageIndicator.isDisplayed()
+            }
+        }
+    }
+
+    @Test
+    fun bottomButtonContinueTest() {
+        run {
+            step("Проверяем отображение кнопки 'Skip'") {
+                OnboardingScreen.forwardButton.isDisplayed()
+            }
+
+            step("Проверяем кликабельность кнопки 'Skip'") {
+                OnboardingScreen.forwardButton.isClickable()
+            }
+        }
+    }
+
+    @Test
+    fun bottomButtonGetStartedTest() {
+        run {
+            step("Убеждаемся, что ее нет на начальном экране") {
+                OnboardingScreen.doneButton.isGone()
+            }
+
+            step("Делаем 3 клика до ее появления") {
+                repeat(numOfPage) {
+                    OnboardingScreen.forwardButton.click()
+                }
+            }
+
+            step("Проверяем отображение кнопки 'Get Started'") {
+                OnboardingScreen.doneButton.isDisplayed()
+            }
+
+            step("Проверяем кликабельность кнопки 'Get Started'") {
+                OnboardingScreen.doneButton.isClickable()
+            }
+        }
+    }
+
+    @Test
+    fun addLanguageTest() {
+        run {
+            step("Нажимаем на кнопку 'Add or edit languge'") {
+                OnboardingScreen.slider.childAt<OnboardingPagerItem>(0) {
+                    addLanguageButton.click()
+                }
+            }
+            step("Проверяем, что открылся экран выбора языков") {
+                WikipediaLanguagesScreen.toolbar.isDisplayed()
+            }
+
+            step("Проверяем наличие списка языков") {
+                WikipediaLanguagesScreen.languageList.isDisplayed()
+            }
+
+            step("Проверяем наличие кнопки 'Add language'") {
+                WikipediaLanguagesScreen.addLanguageButton.isDisplayed()
+            }
+            step("Кликаем кнопку 'Add language'") {
+                WikipediaLanguagesScreen.addLanguageButton.click()
+            }
+            step("Проверяем, что открылся список языков") {
+                AddLanguageScreen {
+                    languageList.childAt<ListLanguageItem>(3) {
+                        localizedLanguageName.isDisplayed()
+                        languageSubtitle.isDisplayed()
+                    }
+                }
+            }
+            step("Кликаем по языку") {
+                AddLanguageScreen {
+                    languageList.childAt<ListLanguageItem>(3) {
+                        localizedLanguageName.click()
+                    }
+                }
+            }
+            step("Возвращаемся на главный экран") {
+                Espresso.pressBack()
+            }
+            step("Проверяем, что язык отобразился на главном экране") {
+                OnboardingScreen.slider.childAt<OnboardingPagerItem>(0) {
+                    languages.childAt<OptionLanguageItem>(2) {
+                        containsText("Deutsch")
+                    }
+                }
+            }
+        }
+    }
 }
 
 
