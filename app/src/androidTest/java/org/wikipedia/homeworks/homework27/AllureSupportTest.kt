9937eb599887a8d5dfda2204ef28336b1a848783
st412m <staroverm@gmail.com>
2025-05-07 08:08:40 +0300

    homework27

--- app/src/androidTest/java/org/wikipedia/homeworks/homework27/AllureSupportTest.kt
+++ app/src/androidTest/java/org/wikipedia/homeworks/homework27/AllureSupportTest.kt
@@ -4,12 +4,16 @@ import com.kaspersky.components.alluresupport.interceptors.step.ScreenshotStepIn
 import com.kaspersky.components.alluresupport.withForcedAllureSupport
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import org.wikipedia.homeworks.tools.allureinterceptors.FailOnlyScreenshotStepInterceptor
+import org.wikipedia.homeworks.tools.allureinterceptors.SuccessFinaleScreenshotTestInterceptor
 
 class AllureSupportTest : TestCase(
     kaspressoBuilder = Kaspresso.Builder.withForcedAllureSupport().apply {
         stepWatcherInterceptors.removeIf {
             it is ScreenshotStepInterceptor
         }
+        stepWatcherInterceptors.add(FailOnlyScreenshotStepInterceptor(screenshots))
+        testRunWatcherInterceptors.add(SuccessFinaleScreenshotTestInterceptor(screenshots))
     }
 )
 
